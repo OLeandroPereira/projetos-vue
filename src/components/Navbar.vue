097de889026489cf1b4453e6d366913f151fa986
Leandro Pereira <https://github.com/OLeandroPereira/site-vue.git>
2022-12-02 00:11:27 -0300

    ajuste layout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,36 +2,29 @@
   <nav>
     <v-toolbar flat app class="mb-13 pa-xl-4">
     <v-img
-          alt="Insure Logo"
-          class="shrink mr-2"
-          contain
-          src="https://insure-landing-page-flax.vercel.app/img/logo.1bc452ac.svg"
-          width="150"
-          height="300"
+      alt="Insure Logo"
+      class="shrink mr-2"
+      contain
+      src="https://insure-landing-page-flax.vercel.app/img/logo.1bc452ac.svg"
+      width="150"
+      height="300"
     />
-    
-      <v-spacer></v-spacer>
-    
+    <v-spacer></v-spacer>
     <v-btn text>
       HOW WE WORK
     </v-btn>
-
     <v-btn text>
       BLOG
     </v-btn>
-
     <v-btn text>
       ACCOUNT
     </v-btn>
-
     <v-btn
       class="ma-2"
       outlined
-      
     >
       VIEW PLANS
     </v-btn>
-    
     </v-toolbar>
   </nav>
 </template>
--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,86 +1,38 @@
-<!--<template>
-
-<div>
-  <v-flex>
+<template>
+  <div>
     <v-card
-      md-6
-      class="mx-auto"
-      height="600"
-      max-width="100%"
-      color="#2d2640"
-    >
-    
-    <v-col cols="12" sm="4">
-      <h1>Humanizing your insurance.</h1>
-      <p >Get your life insurance coverage easier and faster. We blend our expertise and technology to help you find the plan that’s right for you. Ensure you and your loved ones are protected.</p>
-
-      <v-btn
+        class="d-flex"
+        height="625"
+        max-width="100%"
+        color="#2d2640"
+      >
+      <div style="margin: 100px 180px; padding: 0 30px 0; text-align: center">
+        <h1 style="font-family: DMSerif; font-size: 60px; margin: 20px">Humanizing your insurance.</h1>
+        <p>Get your life insurance coverage easier and faster. We blend our expertise and technology to help you find the plan that’s right for you. Ensure you and your loved ones are protected.</p>
+        <v-btn
         class="ma-2"
         outlined
       >
         VIEW PLANS
       </v-btn>
-    </v-col>
-
-    <v-col cols="12" sm="10">
-      <v-img
-      src="https://insure-landing-page-flax.vercel.app/img/image-intro-desktop.8ac69ec7.jpg"
-      class="ml-auto"
-      elevation="12"
-      height="580"
-      max-width="500"
-      >
-        
-      </v-img>
-    </v-col>
-  </v-card>
-    
-    </v-flex>
-</div>
-    
-</template>
--->
-
-<template>
-  <div>
-    <v-card
-      height="625"
-      max-width="100%"
-      color="#2d2640"
-    >
-      <v-row>
-        
-        <v-col cols="12" sm="5" class="mt-16 pa-md-16 m-lg-auto white--text">
-          <h1>Humanizing your insurance.</h1>
-          <p>Get your life insurance coverage easier and faster. We blend our expertise and technology to help you find the plan that’s right for you. Ensure you and your loved ones are protected.</p>
-        </v-col>
-        
-        
-        <v-col cols="12" md="7">
-          <v-img
-            src="https://insure-landing-page-flax.vercel.app/img/image-intro-desktop.8ac69ec7.jpg"
-            class="mt-lg-16"
-            elevation="12"
-            height="580"
-            max-width="500"
-            >
-              
-          </v-img>
-        </v-col>
-      </v-row>
-      <v-flex class="mt-16">
-        <h1>We’re different</h1>
-      </v-flex>
-      
+      </div>
+      <v-col cols="6" md="6">
+        <v-img style="margin-top: 200px"
+          src="https://insure-landing-page-flax.vercel.app/img/image-intro-desktop.8ac69ec7.jpg"
+          height="580"
+          max-width="500"
+          >
+        </v-img>
+      </v-col>
     </v-card>
+    <v-col cols="12" md="10">
+      <h1>We’re different</h1>
+    </v-col>
+    
+    
   </div>
 </template>
 
-
-
-<style>
-
-</style>
 <script>
 export default {
 name
@@ -89,18 +41,5 @@ name
 
 
 <style>
-  h1 {
-    
-    font-family: "DMSerif";
-    font-size: 4rem;
-    text-align: center;
-    margin: 0;
-    
-  }
-
-  p {
-    
-    text-align: center;
-    margin: 0;
-  }
+  
 </style>
